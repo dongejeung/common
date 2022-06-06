230bda2b268e35a587c1214702913d65dea2e1cd
dongejeung <kdj00001@naver.com>
2022-06-06 23:59:59 +0900

    init default_class.py

---
+++ python/default_class.py
@@ -0,0 +1,23 @@
+import sys, os
+
+
+class DefaultClass(self):
+
+    # Init Method
+    def __init__(self):
+        pass
+
+
+    # Main Method
+    def main(self):
+        pass
+
+
+    # Execute Main Method
+    if __name__ == "__main__":
+
+        self.main()
+
+
+
+
